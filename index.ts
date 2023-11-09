e9bb9da00a8f3970cbb8e4f204f0a92adce51511
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2023-11-09 17:19:10 +0700

    style: add prettier

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome Friends')
-})
+  res.send('Welcome Friends');
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
