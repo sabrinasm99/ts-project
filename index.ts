d180a23040fbfc312240bbcc9114c8a3c14c28cd
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2023-11-13 20:49:38 +0700

    feat: connect db

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import express, { Request, Response } from 'express';
+import { connectDB } from './connect';
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome Friends');
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const port = process.env.APP_PORT || 5000;
 
 process.on('SIGINT', () => {
   console.log('Close the app...');
   process.exit();
 });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome Friends');
+});
+
+(async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})();
