a56cd8d8c71d697600632d052d04076e994c3cd5
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2023-11-17 19:43:19 +0700

    feat: migrate db

--- connect.ts
+++ connect.ts
@@ -1,18 +1,20 @@
 import { Client } from 'pg';
 import 'dotenv/config';
 
+export const clientProp = {
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE
+};
+
+export const client = new Client(clientProp);
+
 export const connectDB = async () => {
-  const client = new Client({
-    host: process.env.PG_HOST,
-    port: Number(process.env.PG_PORT),
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
-  });
   await client.connect();
   const res = await client.query('SELECT $1::text as connected', [
     'Connection to postgres successful!'
   ]);
   console.log(res.rows[0].connected);
-  await client.end();
 };
---
+++ migrate/knexfile.ts
@@ -0,0 +1,42 @@
+import type { Knex } from 'knex';
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: 'migrations',
+      tableName: 'knex_migrations'
+    }
+  }
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
+};
+
+module.exports = config;
