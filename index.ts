6311762bd8b8abe6806f0539da02ea52a8340ac7
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2023-11-17 19:43:47 +0700

    feat: insert doctor

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express';
-import { connectDB } from './connect';
+import express, { Request, Response, json } from 'express';
+import { client, connectDB } from './connect';
+import { v4 } from 'uuid';
 
 const app = express();
 const port = process.env.APP_PORT || 5000;
@@ -9,8 +10,25 @@ process.on('SIGINT', () => {
   process.exit();
 });
 
+app.use(json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome Friends');
+});
+
+app.post('/insert', async (req: Request, res: Response) => {
+  const id = v4();
+  const { name, experience } = req.body;
+
+  const text =
+    'INSERT into doctors(id, name, experience) VALUES($1, $2, $3) RETURNING *';
+  const value = [id, name, experience];
+
+  await client.query(text, value);
+
+  res.json({
+    message: 'SUCCESS INSERT'
+  });
 });
 
 (async () => {
